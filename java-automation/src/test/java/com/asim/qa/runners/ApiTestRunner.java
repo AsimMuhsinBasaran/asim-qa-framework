c840f3769101a99c3e94423589145724ed3368fb
Asım Muhsin Basaran <basaranasim@Asm-MacBook-Pro.local>
2026-05-16 16:46:03 +0300

    Enable 2-thread parallel API scenario execution

--- java-automation/src/test/java/com/asim/qa/runners/ApiTestRunner.java
+++ java-automation/src/test/java/com/asim/qa/runners/ApiTestRunner.java
@@ -2,6 +2,7 @@ package com.asim.qa.runners;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
         features = "src/test/resources/features",
@@ -19,4 +20,10 @@ import io.cucumber.testng.CucumberOptions;
         monochrome = true
 )
 public class ApiTestRunner extends AbstractTestNGCucumberTests {
+
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 }
